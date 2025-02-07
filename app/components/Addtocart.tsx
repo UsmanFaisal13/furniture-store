61b7815516cb961383e36ceaad402ee806ac84fa
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2025-02-07 20:06:11 +0500

    cart added

--- app/components/Addtocart.tsx
+++ app/components/Addtocart.tsx
@@ -2,24 +2,23 @@
 import { addToCart } from "../actions/actions"
 import { Product } from "@/types/products"
 import Swal from 'sweetalert2'
-
 export function CartButton({ product }: { product: Product }) {
-    const handleAddToCart = (e: React.MouseEvent) => {
-        e.preventDefault()
-        Swal.fire({
-            position: 'top',
-            title: `${product.name} added to cart`,
-            icon: 'success',
-            showConfirmButton: false,
-            timer: 1500
-        })
-        addToCart(product)
-
-    }
     return (
-
-        <button className='text-xl w-[215px] h-16 rounded-[15px] border-[1px] border-black' onClick={handleAddToCart}>Add to cart</button>
-
+        <button
+            onClick={(e: React.MouseEvent) => {
+                e.preventDefault();
+                Swal.fire({
+                    position: 'top',
+                    title: `${product.name} added to cart`,
+                    icon: 'success',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                addToCart(product);
+            }}
+            className='text-xl w-[215px] h-16 rounded-[15px] border-[1px] border-black'
+        >
+            Add to cart
+        </button>
     )
-
 }
